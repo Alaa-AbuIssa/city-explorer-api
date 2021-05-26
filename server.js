6e747397a1347135ef4a7577c1cbc2c04d1a26a2
Alaa-AbuIssa <alaaabuissa95@gmail.com>
2021-05-26 23:18:46 +0300

    last update for lab7

--- server.js
+++ server.js
@@ -1,57 +1,62 @@
 'use strict';
 
-
-
 const express = require('express');
-const weather = require('./assets/weather.json')
+const weather = require('./assets/weather.json');
+const cors = require ('cors');
 require('dotenv').config();
 
 const server = express();
+server.use(cors());
 
 const PORT = process.env.PORT;
 
 
 
 server.listen(PORT,()=>{
-
-    console.log(`Listing for request!! from your ${PORT}`);
+    console.log(`Listing for request!! from your ${process.env.PORT} PORT`);
 })
 
 
-
-
-// class Forecast {
-//     date = 66565;
-//     description = "";
-// }
-
-
-// let forecastObjArray ={};
-
-// server.get('/weather',(req,res)=>{
-//     let city = weather.map(item=>{
-//         return Math.max.apply(null, item);
-
-//     })
-//     res.send(city)
-//     console.log(city);
-// })
-
+class ForeCast {
+    constructor(object) {
+        this.date = object.valid_date; 
+        this.description = `Low of ${object.low_temp}, high of ${object.max_temp} with ${object.weather.description}`;
+    }
+}
 
 
 // http://localhost:3001/weather?lat= 47.60621&lon=-122.33207&searchQuery=Seattle
 server.get('/weather',(req,res)=>{
-    console.log(req.query);
-    let city = weather.find(item=>{
-        if(item.city_name==req.query.searchQuery && item.lat==req.query.lat&& item.lon==req.query.lon)
-        return item
+    // console.log(req.query);
+
+    let cityData = weather.find(item=>{
+        if(item.city_name.toLocaleLowerCase()==req.query.searchQuery.toLocaleLowerCase() && item.lat==req.query.lat && item.lon==req.query.lon){
+            
+            return item
+        }
         
     })
-    res.send(city)
+    // res.send(cityData)
     // console.log(weather);
+
+
+    try {
+
+        let forecasts = cityData.data.map(item => {
+            return new ForeCast(item);
+        })
+        // console.log(forecasts);
+        res.send(forecasts);
+    } 
+    catch {
+        res.status(404).send('Sorry! there is no city with your informations');
+    };
 })
 
 
 server.get('*',(req,res)=>{
-    res.status(404).send('Sorry! Your Page Not Found')
+    res.status(500).send('Sorry! Your Page Not Found')
 })
+
+
+
